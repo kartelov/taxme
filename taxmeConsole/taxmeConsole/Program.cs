08a3252f85e8cb2f8469685a4accee99c4cb21d6
iAn <ian lenovo>
2020-04-07 01:38:32 +0300

    tue

--- taxmeConsole/taxmeConsole/Program.cs
+++ taxmeConsole/taxmeConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace taxmeConsole
 {
@@ -10,9 +12,33 @@ namespace taxmeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello there");
-            Console.ReadLine();
-            
+            try
+            {
+                // Build connection string
+                //SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                //builder.DataSource = "localhost";   // update me
+                //builder.UserID = "sa";              // update me
+                //builder.Password = "your_password";      // update me
+                //builder.InitialCatalog = "master";
+
+                
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SqlDataBase1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
         }
+
     }
 }
+
